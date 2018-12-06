e1e92967942f76369e9f8f7eee1d01065f37ccaa
victoriak94 <vkamila6@gmail.com>
2018-12-05 20:20:12 -0500

    Created create, edit, and update methods. Redirected create and update methods to entries.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,7 +4,28 @@
 class EntriesController < ApplicationController
 
   def index
-    
+    render :index
   end
 
+  def show
+    render :show
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    redirect_to entries_url
+  end
+
+  def edit
+    render :edit
+  end
+
+  def update
+    redirect_to entry_url(params[:id])
+  end
+
+  
 end
