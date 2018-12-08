6be329c87cce371df699649cbf01ed23b3ee0122
victoriak94 <vkamila6@gmail.com>
2018-12-08 16:38:01 -0500

    Create edit form, edit controller methods to include instance variables. Complete assignment

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,31 +4,55 @@
 class EntriesController < ApplicationController
 
   def index
-    render :index
+    @entries = Entry.all
   end
 
   def show
-    render :show
+    @entry = Entry.find(params[:id])
   end
 
   def new
-    render :new
+    @entry = Entry.new
   end
 
   def create
-    redirect_to entries_url
+    @entry = Entry.new
+
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      render: new
+    end
+
   end
 
   def edit
-    render :edit
+    @entry = Entry.find(params[:id])
   end
 
   def update
-    redirect_to entry_url(params[:id])
+    @entry = Entry.find(params[:id])
+
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      render: new
+    end
+
   end
 
   def destroy
-    redirect_to :index
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_url
   end
 
 end
